fb5a3ac60b1fb71090fa39c9cef46835982cf337
Айрат Молотков <airat@promo-z.ru>
2022-07-24 21:02:57 +0400

    add caclulation figure area

---
+++ FigureArea/Triangle.cs
@@ -0,0 +1,28 @@
+﻿namespace FigureArea;
+
+public class Triangle : IAreaFigure
+{
+    public Triangle(List<double> sides)
+    {
+        Sides = sides;
+    }
+
+    public List<double> Sides { get; set; }
+
+    public double GetArea()
+    {
+        if (Sides.Count != 3)
+            throw new Exception("Incorrect shape data");
+
+        var halfPerimetr = Sides.Sum() / 2;
+
+        return Math.Sqrt(halfPerimetr * (halfPerimetr - Sides[0]) * (halfPerimetr - Sides[1]) * (halfPerimetr - Sides[2]));
+    }
+
+    public bool IsRectangular()
+    {
+        var hypotenuse = Sides.Max();
+
+        return Math.Pow(hypotenuse, 2) == Sides.Where(x => x != hypotenuse).Sum(x => Math.Pow(x, 2));
+    }
+}
